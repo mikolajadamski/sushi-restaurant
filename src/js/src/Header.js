28657da6dcbb45e42e2f5fbb91512e63ba632119
mikolajadamski <mikolaj.adamski47@gmail.com>
2021-03-06 13:40:24 +0100

    add navbar

--- src/js/src/Header.js
+++ src/js/src/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Header.css';
-
+import Navbar from './Navbar';
 
 const Header = (props) => {
     return (
-        <div>Hello world</div>
+        <Navbar />
     )
 }
 
---
+++ src/js/src/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = (props) => {
+    return (
+        <div id="navbar">
+            <ul>
+                <li>
+                    Home
+                </li>
+                <li>MOnke</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar;
