269edb6763cf4047d3973bfb1303e903a265fd2e
mikolajadamski <mikolaj.adamski47@gmail.com>
2021-03-06 13:15:07 +0100

    add js

---
+++ src/js/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+
+const App = (props) => {
+  return (
+    <Header />
+  );
+}
+
+export default App;
---
+++ src/js/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+
+
+const Header = (props) => {
+    return (
+        <div>Hello world</div>
+    )
+}
+
+export default Header;
